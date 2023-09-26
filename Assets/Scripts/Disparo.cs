e567c2987580dee51154d39dfef15fe11da28839
TSC2002 <santacruztomas4@gmail.com>
2023-09-25 22:08:43 -0300

    Se hizo para que se pueda disparar

---
+++ Assets/Scripts/Disparo.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+    public GameObject Bala;
+    public Transform SpawnDeLaBala;
+
+    public float FuerzaDeTiro = 1500f;
+    public float FuerzaDeRotacion = 0.5f;
+
+    private float TiempoDeDisparo = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Time.time > TiempoDeDisparo)
+            {
+                GameObject NuevaBala;
+
+                NuevaBala = Instantiate(Bala, SpawnDeLaBala.position, SpawnDeLaBala.rotation);
+
+                NuevaBala.GetComponent<Rigidbody>().AddForce(SpawnDeLaBala.forward * FuerzaDeTiro);
+
+                TiempoDeDisparo = Time.time + FuerzaDeRotacion;
+
+                Destroy(NuevaBala, 2);
+            }
+        }
+    }
+}
